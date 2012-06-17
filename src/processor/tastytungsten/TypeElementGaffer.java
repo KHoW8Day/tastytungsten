8480fc36a221b90a6a4ffd5ec82c317955fb4645
Emory Hughes Merryman III <emory@sillygoose.solidyard>
2012-06-16 17:50:11 -0800

    Emory is considering changing the writer to reduce the number of parameters.

--- src/processor/tastytungsten/TypeElementGaffer.java
+++ src/processor/tastytungsten/TypeElementGaffer.java
@@ -1,3 +1,20 @@
+// Copyright © (C) 2012 Emory Hughes Merryman, III
+//
+// This file is part of tastytungsten.
+//
+// tastytungsten is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// tastytungsten is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 package tastytungsten ;
 
@@ -7,13 +24,30 @@ import javax . lang . model . type . TypeMirror ;
 import javax . lang . model . type . TypeVisitor ;
 import javax . lang . model . util . SimpleElementVisitor6 ;
 
-abstract class TypeElementGaffer < R , P , A , B > extends SimpleElementVisitor6 < R , P >
+/**
+ * A visitor that connects 2 visitors.
+ *
+ * @param <R> the return type of the visitor
+ * @param <P> the data type of the visitor
+ * @param <A> the wrangler data type
+ * @param <B> the visitor data type
+ **/
+abstract class TypeElementGaffer < R , P , A , B >
+    extends SimpleElementVisitor6 < R , P >
 {
+    /**
+     * Connects 2 visitors - a wrangler and a visitor.
+     *
+     * @param element the element to visit
+     * @param data user data
+     * @return the result of the visitor
+     **/
     @ Override
-	protected R defaultAction ( Element element , P data )
+	protected R defaultAction ( final Element element , final P data )
 	{
 	    TypeVisitor < ? extends R , ? super B > visitor = getVisitor ( ) ;
-	    ElementVisitor < ? extends TypeMirror , ? super A > wrangler = getWrangler ( ) ;
+	    ElementVisitor < ? extends TypeMirror , ? super A > wrangler =
+		getWrangler ( ) ;
 	    A a = getA ( data ) ;
 	    TypeMirror type = wrangler . visit ( element , a ) ;
 	    B b = getB ( data ) ;
@@ -21,15 +55,40 @@ abstract class TypeElementGaffer < R , P , A , B > extends SimpleElementVisitor6
 	    return visit ;
 	}
 
+    /**
+     * Get the wrangler.
+     *
+     * @return the wrangler
+     **/
     @ UseParameter
-	abstract ElementVisitor < ? extends TypeMirror , ? super A > getWrangler ( ) ;
+	abstract
+	ElementVisitor < ? extends TypeMirror , ? super A >
+	getWrangler
+	( ) ;
 
+    /**
+     * Get the visitor.
+     *
+     * @return the visitor
+     **/
     @ UseParameter
 	abstract TypeVisitor < ? extends R , ? super B > getVisitor ( ) ;
 
+    /**
+     * Get wrangler data.
+     *
+     * @param data user data
+     * @return wrangler data
+     **/
     @ UseNull
 	abstract A getA ( P data ) ;
 
+    /**
+     * Get visitor data.
+     *
+     * @param data user data
+     * @return visitor data
+     **/
     @ UseNull
 	abstract B getB ( P data ) ;
 }
--- src/processor/tastytungsten/TypeElementWrangler.java
+++ src/processor/tastytungsten/TypeElementWrangler.java
@@ -1,3 +1,20 @@
+// Copyright © (C) 2012 Emory Hughes Merryman, III
+//
+// This file is part of tastytungsten.
+//
+// tastytungsten is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// tastytungsten is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 package tastytungsten ;
 
@@ -5,18 +22,29 @@ import javax . lang . model . element . Element ;
 import javax . lang . model . type . TypeMirror ;
 import javax . lang . model . util . SimpleElementVisitor6 ;
 
-abstract class TypeElementWrangler < P > extends SimpleElementVisitor6 < TypeMirror , P >
+/**
+ * Gets the type of an element.
+ *
+ * @param <P> user data type
+ **/
+abstract class TypeElementWrangler < P >
+    extends SimpleElementVisitor6 < TypeMirror , P >
 {
     /**
      * Gets the type of an element.
      *
+     * @param element the element
+     * @param data user data
      * @return the element type
-     * @
      **/
     @ Override
-	protected TypeMirror defaultAction ( Element element , P data )
+	protected
+	TypeMirror
+	defaultAction
+	( final Element element , final P data )
 	{
 	    TypeMirror type = element . asType ( ) ;
 	    return type ;
 	}
 }
+
