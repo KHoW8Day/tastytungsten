22785114b32b9c0f0df69cc4e75f067a39524e73
Emory Hughes Merryman III <emory@sillygoose.solidyard>
2012-06-19 20:24:52 -0800

    Emory got junit to work initially.

---
+++ src/junit/tastytungsten/MyTests.java
@@ -0,0 +1,18 @@
+
+package tastytungsten ;
+
+import org . junit . * ;
+import static org . junit . Assert . * ;
+
+public class MyTests
+{
+    public MyTests ( )
+    {
+    }
+
+    @ Test
+	public void easyPass ( )
+    {
+	assertTrue ( true ) ;
+    }
+}
