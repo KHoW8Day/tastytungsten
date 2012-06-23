d17983ede35369b99bf18f5273b282a5ade5763a
Emory Hughes Merryman III <emory@sillygoose.solidyard>
2012-06-23 03:37:11 -0800

    Emory changes the checks.

--- git.sh
+++ git.sh
@@ -1,4 +1,4 @@
-git add src/*/tastytungsten/*.java  src/checks.xml src/header.txt build.xml git.sh
+git add src/*/tastytungsten/*.java  src/*.xml src/header.txt build.xml git.sh
 git commit
 git push
 
